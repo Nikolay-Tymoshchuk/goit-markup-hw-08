d9e9c55e929e2530f2b59b4aec972c7aa5de72cf
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2021-12-24 01:55:05 +0200

    fix issues after check

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,63 @@
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]");
+
+//   const mobileMenuRef = document.querySelector("[data-menu]");
+
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded =
+//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+//     menuBtnRef.classList.toggle("is-open");
+//     menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+//     mobileMenuRef.classList.toggle("is-open");
+//   });
+
+// })();
+
+// (() => {
+ 
+//   const openModalBtn = document.querySelector('[data-menu-open]');
+
+//   const closeModalBtn = document.querySelector('[data-menu-close]'); 
+
+//   const mobileMenuRef = document.querySelector("[data-menu]");
+
+//       openModalBtn.addEventListener('click', toggleModal);
+//       closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     const expanded =
+//       openModalBtn.getAttribute("aria-expanded") === "true" || false;
+
+//       openModalBtn.classList.toggle("is-open");
+//       openModalBtn.setAttribute("aria-expanded", !expanded);
+    
+//       closeModalBtn.getAttribute("aria-expanded") === "true" || false;
+
+//       closeModalBtn.classList.toggle("is-open");
+//       closeModalBtn.setAttribute("aria-expanded", !expanded);
+
+//       mobileMenuRef.classList.toggle("is-open");
+//   }
+
+// })();
+
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
-  });
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.menu.classList.toggle('is-open');
+  }
 })();
+
+
+
